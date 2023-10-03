8165d002ab52c6a4ec89442a69bb98665be7e52e
abhishek781 <20abhishek16@gmail.com>
2023-10-03 18:10:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Updated import statements
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import History from './pages/History';
+import WordDetailsPage from './pages/WordDetailsPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes> {/* Use Routes instead of Switch */}
+          <Route path="/" element={<Home />} /> {/* Use element property */}
+          <Route path="/history" element={<History />} /> {/* Use element property */}
+          <Route path="/word/:word" element={<WordDetailsPage />} /> {/* Use element property */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+
